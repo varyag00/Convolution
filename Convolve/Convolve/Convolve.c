3d1c6f1f9633afc4971cf65426e0d4af67f2fde5
Dan Gonzalez <d.gonzalez0902@gmail.com>
2015-12-07 14:40:43 -0700

    Runtime argument parsing complete

--- Convolve/Convolve/Convolve.c
+++ Convolve/Convolve/Convolve.c
@@ -1,12 +1,59 @@
+/*
+Name:		J. Daniel Gonzalez
+UCID:		10058656
+Class:		CPSC 501
+Ass:		4
+
+Notes:		Program implements convolution reverb to a dry sound with an inpulse response, and outputs the sound as if it were playing in a concert hall.
+
+Sources:	
+*/
+
+
 #include<stdio.h>
 
-int main(){
-	printf("hello world\n");
+#pragma region Macros
 
+#define REQ_ARGS 4
+
+#pragma endregion
+
+#pragma region Attributes
+
+char* usageStr = "Usage: convolve <input file> <IRfile> <output file> ";
+char* inputFile;
+char* IRFile;
+char* outputFile;
+
+#pragma endregion 
+
+
+int main(int argc, char *argv[]){
+	
+	//check arguments
+	if (argc != REQ_ARGS){
+		printf(usageStr);
+		return 0;
+	}
+
+	//set args
+	inputFile = argv[1];
+	IRFile = argv[2];
+	outputFile = argv[3];
+
+	//TODO: delete this after debugging
+	printf("input file: %s, IR file: %s, output file: %s\n", inputFile, IRFile, outputFile);		
+
+	//read .wav files
+
+	
+
+	//perform convolution
+	
 
 	getchar();		//keeps terminal open (visual studio) 
-	return 0;
 }
+
 
 
 
